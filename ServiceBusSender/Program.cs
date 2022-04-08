6f18e3a40e4ff348fb1f8a1bb261b2789ebf44f5
Annu Priya <Annu_Priya@epam.com>
2022-04-08 16:45:52 +0530

    Add project files.

---
+++ ServiceBusSender/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusSender
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "";
+        const string queuename = "queue1";
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, queuename);
+            await SendMessage(numberOfMessages);
+            await queueClient.CloseAsync();
+        }
+        static async Task SendMessage(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for(int i=0; i< numberOfMessagesToSend; i++)
+                {
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                    Console.WriteLine($"Sending Message : {messageBody}");
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
